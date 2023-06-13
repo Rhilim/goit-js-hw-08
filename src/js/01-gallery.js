20780491e273ea4eee29562740be30caf28c4a0d
Rhilim <arhilim@gmail.com>
2023-06-13 07:34:07 -0500

    task 1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,73 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// require ('simplelightbox');
+
+
+const createDynamicGallery = document.querySelector(".gallery");
+createDynamicGallery.style.listStyle = 'none';
+const picturesMarkup = createDynamicGalleryMarkup(galleryItems);
+createDynamicGallery.insertAdjacentHTML("beforeend", picturesMarkup);
+
+createDynamicGallery.addEventListener("click", onGalleryClick);
+
+function createDynamicGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+    })
+    .join("");
+}
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+
+  let createDynamicGallery = new SimpleLightbox(".gallery a", {
+    captions: true,
+    // captionSelector: "alt",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 console.log(galleryItems);
