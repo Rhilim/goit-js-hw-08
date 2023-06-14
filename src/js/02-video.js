76522de1d150ce888df279b2e28660289d4e86f4
Rhilim <arhilim@gmail.com>
2023-06-14 18:37:34 -0500

    task 3 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+{
+  /* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe> */
+}
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const formInput = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+const formData = {};
+outputToFormData();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+
+  const message = JSON.stringify(formData);
+  //   console.log(message);
+  localStorage.setItem('feedback-form-state', message);
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  const parsedMsg = JSON.parse(savedMessage);
+  console.log(parsedMsg);
+
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function outputToFormData() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  const parsedMsg = JSON.parse(savedMessage);
+  if (savedMessage) {
+    formInput.value = parsedMsg.email;
+    textarea.value = parsedMsg.message;
+  }
+}
